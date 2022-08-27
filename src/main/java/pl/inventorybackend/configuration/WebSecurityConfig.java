ed82048445ab46a334a67ceddcc177893fb248af
AWieczorek00 <awieczorek0000@gmail.com>
2022-08-27 13:04:12 +0200

    add crosingOrigin

--- src/main/java/pl/inventorybackend/configuration/WebSecurityConfig.java
+++ src/main/java/pl/inventorybackend/configuration/WebSecurityConfig.java
@@ -1,18 +1,20 @@
 package pl.inventorybackend.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import pl.inventorybackend.security.session.SessionFilter;
 import pl.inventorybackend.security.User.UserService;
 
@@ -27,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SessionFilter sessionFilter;
     private final PasswordEncoder passwordEncoder;
 
-    public WebSecurityConfig(UserService userService, SessionFilter sessionFilter,@Lazy PasswordEncoder passwordEncoder) {
+    public WebSecurityConfig(UserService userService, SessionFilter sessionFilter, @Lazy PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.sessionFilter = sessionFilter;
         this.passwordEncoder = passwordEncoder;
@@ -71,5 +73,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 UsernamePasswordAuthenticationFilter.class
         );
     }
+
+//    @Bean
+//    public FilterRegistrationBean corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//        bean.setOrder(0);
+//        return bean;
+//    }
 }
 
--- src/main/java/pl/inventorybackend/security/restAuthController/AuthenticationController.java
+++ src/main/java/pl/inventorybackend/security/restAuthController/AuthenticationController.java
@@ -5,16 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.inventorybackend.dto.ResponseDTO;
 import pl.inventorybackend.dto.UserDTO;
 import pl.inventorybackend.security.session.SessionRegistry;
 
-
+@RequestMapping("/api")
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     public final AuthenticationManager manager;
